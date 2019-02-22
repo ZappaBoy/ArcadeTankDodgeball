7e38fe5d170cba14197469957c816d8ee73cdded
Federico <federico_zappone@hotmail.it>
2019-02-22 16:31:56 +0100

    update 6.8 -- add final background

--- src/GUI/Level_Final.java
+++ src/GUI/Level_Final.java
@@ -2,9 +2,6 @@ package GUI;
 
 
 import Enemies.Enemy;
-import Enemies.Enemy_Level1;
-import Enemies.Enemy_Level2;
-import Enemies.Enemy_Level3;
 
 
 
@@ -13,15 +10,15 @@ public class Level_Final extends Level {
     Level_Final() {
         gameLevel = 4;
 
-        enemyLevel1_number = 1;
-        enemyLevel2_number = 1;
-        enemyLevel3_number = 1;
+//        enemyLevel1_number = 1;
+//        enemyLevel2_number = 1;
+//        enemyLevel3_number = 1;
 
-        enemies = new Enemy[getEnemies_number()];
+        enemies = new Enemy[1];
 
-        enemies[0] = new Enemy_Level1();
-        enemies[1] = new Enemy_Level2();
-        enemies[2] = new Enemy_Level3();
+      //  enemies[0] = new Enemy_Level1();
+//        enemies[1] = new Enemy_Level2();
+//        enemies[2] = new Enemy_Level3();
     }
 
 }
